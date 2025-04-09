c708baf7e434b76ec6053b1956b4fe750ce05fa5
Lisa-Gizri <glisa50@gmail.com>
2025-04-09 10:23:16 -0400

    commit

--- public/main.js
+++ public/main.js
@@ -1,9 +1,13 @@
+// Load JSON data using jQuery's $.getJSON() method
 $.getJSON('data.json', function (data) {
+    // Access the array of ticket-style cards from the JSON
     const ticketCards = data.ticketCards;
 
+    // Create a new section and a row container using Bootstrap classes
     const section = $('<section class="container mt-4"><div class="row gx-4"></div></section>');
     const row = section.find('.row');
 
+    // Loop through each ticket card in the JSON data
     ticketCards.forEach(card => {
         const col = $('<div class="col-lg-6"></div>');
         const cardContainer = $(`
@@ -19,6 +23,7 @@ $.getJSON('data.json', function (data) {
 
         const list = cardContainer.find('.list-group');
 
+        // Loop through each list item and add it to the card
         card.items.forEach(item => {
             const listItem = $(`
                 <li class="list-group-item">
